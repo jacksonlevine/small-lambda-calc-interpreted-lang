0cfb9339b603795d6b87ac0660de38772c66e33e
jacksonlevine <jack@netbug.us>
2023-08-02 16:42:04 -0700

    format main.cpp

--- main.cpp
+++ main.cpp
@@ -8,45 +8,59 @@
 std::map<std::string, std::function<std::string(std::vector<std::string>)>> funcs;
 std::map<std::string, std::string> variables;
 
-void function_definitions() {
-    funcs["concat"] = [](std::vector<std::string> args) {
+void function_definitions()
+{
+    funcs["concat"] = [](std::vector<std::string> args)
+    {
         std::string result;
-        for(auto& s : args) {
+        for (auto &s : args)
+        {
             result += s;
         }
         return result;
     };
-    funcs["print"] = [](std::vector<std::string> args) {
-        for(auto &s : args) {
+    funcs["print"] = [](std::vector<std::string> args)
+    {
+        for (auto &s : args)
+        {
             std::cout << s << std::endl;
         }
         return std::string("success");
     };
-    funcs["let"] = [](std::vector<std::string> args) {
-        if(args.size() == 1)
+    funcs["let"] = [](std::vector<std::string> args)
+    {
+        if (args.size() == 1)
         {
             variables[args[0]] = std::string("");
         }
-        if(args.size() > 1)
+        if (args.size() > 1)
         {
             variables[args[0]] = std::string(args[1]);
         }
         return std::string("");
     };
-    funcs["+"] = [](std::vector<std::string> args) {
+    funcs["+"] = [](std::vector<std::string> args)
+    {
         bool floats = false;
-        for(auto& s : args) {
-            if(s.find('.') != std::string::npos) floats = true;
+        for (auto &s : args)
+        {
+            if (s.find('.') != std::string::npos)
+                floats = true;
         }
-        if(floats) {
+        if (floats)
+        {
             float out = 0.0f;
-            for(auto& s : args) {
+            for (auto &s : args)
+            {
                 out += std::stof(s);
             }
             return std::to_string(out);
-        } else {
+        }
+        else
+        {
             int out = 0;
-            for(auto& s : args) {
+            for (auto &s : args)
+            {
                 out += std::stoi(s);
             }
             return std::to_string(out);
@@ -59,32 +73,47 @@ void function_definitions() {
     funcs["se"] = funcs["let"];
     funcs["s"] = funcs["let"];
 }
-std::string recurse_and_call_line(std::string line) {
+std::string recurse_and_call_line(std::string line)
+{
     std::istringstream line_stream(line);
     std::string word;
     std::vector<std::string> current_expression;
-    while(line_stream >> word)
+    while (line_stream >> word)
     {
-        if(word.front() == '(') 
+        if (word.front() == '(')
         {
             int depth = std::count(word.begin(), word.end(), '(');
             std::string inner_line = "";
             inner_line += word;
-            while(depth != 0) {
+            while (depth != 0)
+            {
                 line_stream >> word;
                 inner_line = (inner_line + " ") + word;
-                if(word.front() == '(') { depth += std::count(word.begin(), word.end(), '('); }
-                if(word.back() == ')') { depth -= std::count(word.begin(), word.end(), ')'); }
+                if (word.front() == '(')
+                {
+                    depth += std::count(word.begin(), word.end(), '(');
+                }
+                if (word.back() == ')')
+                {
+                    depth -= std::count(word.begin(), word.end(), ')');
+                }
             }
-            inner_line = inner_line.substr(1, inner_line.size()-2);
+            inner_line = inner_line.substr(1, inner_line.size() - 2);
             word = recurse_and_call_line(inner_line);
         }
-        if(word.front() == '.') { word = variables[word.substr(1)]; }
-        if(word.front() == '"') {
+        if (word.front() == '.')
+        {
+            word = variables[word.substr(1)];
+        }
+        if (word.front() == '"')
+        {
             std::string literal = "";
             literal += word.substr(1);
             char ch;
-            while (line_stream.get(ch) && ch != '"') { literal += ch; }
+            while (line_stream.get(ch) && ch != '"')
+            {
+                literal += ch;
+            }
             word = literal;
         }
         current_expression.push_back(word);
@@ -93,14 +122,16 @@ std::string recurse_and_call_line(std::string line) {
     current_expression.erase(current_expression.begin());
     return funcs[fname](current_expression);
 }
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     function_definitions();
     std::ifstream file(argv[1]);
     std::stringstream buf;
     buf << file.rdbuf();
     std::istringstream stream(buf.str());
     std::string line;
-    while(std::getline(stream, line)) {
+    while (std::getline(stream, line))
+    {
         recurse_and_call_line(line);
     }
 }
